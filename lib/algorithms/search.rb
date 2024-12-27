38b57b7fc86fa920c4c5f80b09af3131c9a5121b
Roy Chanunkha <roychanunkha@gmail.com>
2024-12-26 19:56:30 -0800

    chore: interpolation search

--- lib/algorithms/search.rb
+++ lib/algorithms/search.rb
@@ -28,6 +28,25 @@ module Algorithms
       end
       -1
     end
-    # rubocop:enable Metrics/MethodLength
+
+    # rubocop:disable Metrics/AbcSize
+    def self.interpolation(array, value)
+      low = 0
+      high = array.size - 1
+
+      while value >= array[low] && value <= array[high] && low <= high
+        probe = low + (high - low) * (value - array[low]) / (array[high] - array[low])
+
+        return probe if array[probe] == value
+
+        if array[probe] < value
+          low = probe + 1
+        else
+          high = probe - 1
+        end
+      end
+      -1
+    end
+    # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
   end
 end
--- spec/algorithms/search_spec.rb
+++ spec/algorithms/search_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require_relative '../../lib/algorithms/search'
 
 describe Algorithms::Search do
-  let(:array) { [1, 2, 3, 4, 5, 6] }
+  let(:array) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] }
 
   describe '.linear' do
     context 'when the element is in the array' do
@@ -15,7 +15,7 @@ describe Algorithms::Search do
 
     context 'when the element is not in the array' do
       it 'returns -1' do
-        expect(Algorithms::Search.linear(array, 10)).to eq(-1)
+        expect(Algorithms::Search.linear(array, 14)).to eq(-1)
       end
     end
   end
@@ -29,7 +29,21 @@ describe Algorithms::Search do
 
     context 'when the element is not in the array' do
       it 'returns -1' do
-        expect(Algorithms::Search.binary(array, 10)).to eq(-1)
+        expect(Algorithms::Search.binary(array, 14)).to eq(-1)
+      end
+    end
+  end
+
+  describe '.interpolation' do
+    context 'when the element is in the array' do
+      it 'return the index of the element' do
+        expect(Algorithms::Search.interpolation(array, 8)).to eq(7)
+      end
+    end
+
+    context 'when the element is not in the array' do
+      it 'return -1' do
+        expect(Algorithms::Search.interpolation(array, 14)).to eq(-1)
       end
     end
   end
