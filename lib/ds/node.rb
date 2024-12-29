1657cf6da4b052aa703a7e7205101161a64b9f52
Roy Chanunkha <roychanunkha@gmail.com>
2024-12-29 12:33:36 -0800

    chore: avl tree

--- lib/ds/node.rb
+++ lib/ds/node.rb
@@ -3,12 +3,13 @@
 module Ds
   # This class will be used in a linked List. It represents a single node in the list.
   class Node
-    attr_accessor :value, :next, :prev
+    attr_accessor :value, :next, :prev, :height
 
     def initialize(value)
       @prev = nil
       @value = value
       @next = nil
+      @height = 1
     end
 
     def right=(node)
